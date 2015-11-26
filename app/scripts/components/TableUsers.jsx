390649c9303e546ec1192dbf7acf9ebaaff7c856
skok <skok@ciklum.com>
2015-11-26 18:38:05 +0200

    Added table output. Need to make submodules.

---
+++ app/scripts/components/TableUsers.jsx
@@ -0,0 +1,33 @@
+/**
+ * Created by skok on 26.11.15.
+ */
+
+import React from 'react';
+
+export default React.createClass({
+    displayName: 'TableUsers',
+    render: function () {
+        return (
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>User</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>2</td>
+                            <td>Bob</td>
+                        </tr>
+                        <tr>
+                            <td>662</td>
+                            <td>John</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+});
--- app/scripts/index.jsx
+++ app/scripts/index.jsx
@@ -1,4 +1,7 @@
 import React from 'react';  
 import ReactDOM from 'react-dom';  
-import Hello from './components/Hello';  
+import Hello from './components/Hello';
+import TableUsers from './components/TableUsers';
+
 ReactDOM.render(<Hello />, document.getElementById('content'));
+ReactDOM.render(<TableUsers />, document.getElementById('content'));
