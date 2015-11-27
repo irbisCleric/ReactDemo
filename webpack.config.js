f670baf871c1a7a74eef352c248f186018ffd92f
skok <skok@ciklum.com>
2015-11-27 11:08:07 +0200

    Uninstall extract  text plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 var path = require('path');
 var OpenBrowserPlugin = require('open-browser-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: './app/scripts/index.jsx',
