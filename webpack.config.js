0c050a3491089aeaf137ce40a24ee72d7a1c0024
skok <skok@ciklum.com>
2015-11-27 10:02:53 +0200

    Webpack. Updated config. Stylesheet loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: './app/scripts/index.jsx',
@@ -8,18 +9,35 @@ module.exports = {
         filename: "./build/bundle.js"
     },
     plugins: [
-        new OpenBrowserPlugin({ url: 'http://localhost:8080' })
+        new OpenBrowserPlugin({url: 'http://localhost:8080'})
+        //new ExtractTextPlugin("[name].css")
     ],
     module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            loaders: ['react-hot', 'babel'],
-            exclude: /node_modules/
-        }, {
-            test: /\.jsx$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-        }]
+        loaders: [
+            // JSX
+            {
+                test: /\.jsx?$/,
+                loaders: ['react-hot', 'babel'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+
+            // SASS
+            {
+                test: /\.scss$/,
+                loader: 'style!css!sass'
+            },
+
+            // CSS
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }
+        ]
     },
     externals: {
         //don't bundle the 'react' npm package with our bundle.js
