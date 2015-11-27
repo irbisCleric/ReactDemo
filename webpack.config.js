f7ad7dff0c30a5ba09566dd28849c9472df93594
Zhenya <EvgeniyWide@gmail.com>
2015-11-27 22:16:59 +0200

    react-hot-loader enabled

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,11 @@ var path = require('path');
 var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
 module.exports = {
-    entry: './app/scripts/index.jsx',
+    entry: [
+      'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+      'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+      './app/scripts/index.jsx' // Your appʼs entry point
+    ],
     output: {
         path: __dirname,
         filename: "./build/bundle.js"
