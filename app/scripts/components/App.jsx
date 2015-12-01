9bb9bfa22199d2db3faedb0462b10a5bb95392cd
skok <skok@ciklum.com>
2015-12-01 16:38:36 +0200

    Overlay example.

--- app/scripts/components/App.jsx
+++ app/scripts/components/App.jsx
@@ -1,60 +1,23 @@
 import Header from './Header/Header';
 
-import { Button, Modal } from 'react-bootstrap';
-
-let Example = React.createClass({
-    getInitialState() {
-        return {
-            showModal: false
-        };
-    },
-
-    close() {
-        this.setState({
-            showModal: false
-        });
-    },
-
-    open() {
-        this.setState({
-            showModal: true
-        });
-    },
-
-    render() {
-        return (
-            <div>
-                <Button
-                    bsStyle="primary"
-                    bsSize="large"
-                    onClick={this.open}
-                >
-                    Launch demo modal
-                </Button>
-
-                <Modal show={this.state.showModal} onHide={this.close}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Modal heading</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <h4>Text in a modal</h4>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button onClick={this.close}>Close</Button>
-                    </Modal.Footer>
-                </Modal>
-            </div>
-        );
-    }
-});
-
+import { Button, Popover, OverlayTrigger } from 'react-bootstrap';
 
 export default class App extends React.Component {
     render() {
         return (
             <div className="container">
                 <div className="row">
-                    <Example />
+                    <OverlayTrigger
+                        trigger="click"
+                        placement="bottom"
+                        overlay={
+                            <Popover title="Popover bottom" id="some-id">
+                                <strong>Holy guacamole!</strong>
+                                    Check this info.
+                                </Popover>
+                                }>
+                        <Button bsStyle="default">Holy guacamole!</Button>
+                    </OverlayTrigger>
                     <Header/>
                 </div>
                 <div className="row">
