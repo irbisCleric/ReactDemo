eb4765e99175c54e6e406c96ae07a6a078c1b048
skok <skok@ciklum.com>
2015-12-01 15:32:35 +0200

    Modal dialog added.

--- app/scripts/components/App.jsx
+++ app/scripts/components/App.jsx
@@ -1,14 +1,64 @@
 import Header from './Header/Header';
 
+import { Button, Modal } from 'react-bootstrap';
+
+let Example = React.createClass({
+    getInitialState() {
+        return {
+            showModal: false
+        };
+    },
+
+    close() {
+        this.setState({
+            showModal: false
+        });
+    },
+
+    open() {
+        this.setState({
+            showModal: true
+        });
+    },
+
+    render() {
+        return (
+            <div>
+                <Button
+                    bsStyle="primary"
+                    bsSize="large"
+                    onClick={this.open}
+                >
+                    Launch demo modal
+                </Button>
+
+                <Modal show={this.state.showModal} onHide={this.close}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <h4>Text in a modal</h4>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={this.close}>Close</Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        );
+    }
+});
+
+
 export default class App extends React.Component {
     render() {
         return (
             <div className="container">
                 <div className="row">
+                    <Example />
                     <Header/>
                 </div>
                 <div className="row">
-                {this.props.children}
+                    {this.props.children}
                 </div>
             </div>
         )
--- app/scripts/components/Header/Header.jsx
+++ app/scripts/components/Header/Header.jsx
@@ -1,12 +1,12 @@
-import { Router, Route, Link } from 'react-router';
 import './Header.scss';
+
+import { Router, Route, Link } from 'react-router';
 
 export default class Header extends React.Component {
     render() {
         return (
             <div>
                 <h1>Hello React</h1>
-
                 <div className="btn-group">
                     <Link to="/" className="btn btn-primary">Home</Link>
                     <Link to="/users" className="btn btn-primary" activeClassName='active'>Users</Link>
